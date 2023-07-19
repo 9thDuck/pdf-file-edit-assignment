c704c397172f2b3d80595234d8c76f5abe8fb161
7thclass <msai@live.co.uk>
2023-07-19 21:26:10 +0530

    whitelist validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { checkDbForFileAndAddIfNeeded } from './utils/initDb';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   await checkDbForFileAndAddIfNeeded();
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3001);
 }
 bootstrap();
