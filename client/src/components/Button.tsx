d34081ad8cadfe429308aa445b3625fa6856d304
7thclass <msai@live.co.uk>
2023-07-19 19:58:12 +0530

    border styling changed

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -11,8 +11,8 @@ const Button: React.FC<ButtonProps> = ({ text = '', onClick, disabled }) => {
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`rounded border-2 border-solid border-black px-5 py-3 font-medium capitalize ${
-        disabled ? 'bg-gray-400' : 'shadow-sm hover:shadow-xl duration-300 '
+      className={`rounded border-2 border-solid border-gray-500 px-5 py-3 font-medium capitalize ${
+        disabled ? 'bg-gray-300' : 'shadow-sm hover:shadow-xl duration-300 '
       }`}
     >
       {text}
