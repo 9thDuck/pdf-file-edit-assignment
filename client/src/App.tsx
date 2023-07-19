98033be6a0987f84090fa8a0261364637d960ae8
7thclass <msai@live.co.uk>
2023-07-19 19:31:26 +0530

    custom hook init, recoil setup and Topbar moved

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import Topbar from './components/Topbar/Topbar';
+
 function App() {
-  return <div className="">asdf</div>;
+  return (
+    <div className="flex flex-col p-4 w-screen h-screen">
+      <Topbar />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 type ButtonProps = {
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
+  disabled?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ text = '', onClick }) => {
+const Button: React.FC<ButtonProps> = ({ text = '', onClick, disabled }) => {
   return (
     <button
       onClick={onClick}
+      disabled={disabled}
       className="rounded border-2 border-solid border-black px-5 py-3 font-medium capitalize"
     >
       {text}
