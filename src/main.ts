83d652a3a754ef437c8656bd6bab90d9d0530591
7thclass <msai@live.co.uk>
2023-07-19 23:12:38 +0530

    cors functionality enabled

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   await checkDbForFileAndAddIfNeeded();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3001);
 }
