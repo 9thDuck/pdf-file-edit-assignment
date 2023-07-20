565691e5c38ebe2b7031dee78e394d8fc91913fb
7thclass <msai@live.co.uk>
2023-07-20 08:42:16 +0530

    serving vanilla html client with tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { checkDbForFileAndAddIfNeeded } from './utils/initDb';
 import { ValidationPipe } from '@nestjs/common';
+import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   await checkDbForFileAndAddIfNeeded();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors();
+
+  app.useStaticAssets(path.join(__dirname, '..', 'client'), {
+    prefix: '/',
+  });
+
+  app.setViewEngine('html');
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3001);
 }
